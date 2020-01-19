a8b4fb45f80d70512f9c6a33642515daa6ee5710
Giuseppe Eustaquio <gseppe222@gmail.com>
2020-01-19 18:36:08 +0800

    adds links

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
   def hello
+    @date = Date.current
+  end
+
+  def goodbye
+    @time = Time.current
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root "static_pages#hello"
+  get "/goodbye", to: "static_pages#goodbye"
 end
